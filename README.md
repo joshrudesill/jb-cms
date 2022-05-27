e9298d942fa8045aa841f64bf6b1a513151887b7
joshrudesill <63250227+joshrudesill@users.noreply.github.com>
2022-05-27 15:14:52 -0500

    EXPERIMENTING

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Jaras Beauty Content Management System
-#### A front end authenticated portal for the business owner to be able to alter data on the site easily
-#### Working Version
-#### https://regwm9.csb.app/
+# jb-cms
+Created with CodeSandbox
--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
 import { useEffect, useState } from 'react';
 import {db} from './firebase/fb-utils';
 import { collection, getDocs } from "firebase/firestore";
+import _ from 'lodash';
 
 const App = () => {
   const [spanish, setSpanish] = useState();
   const [english, setEnglish] = useState();
+  const [englishUpdated, setEnglishUpdated] = useState();
+  const [spanishUpdated, setSpanishUpdated] = useState();
+  
   const fetch = async () => {
-    
       const querySnap = await getDocs(collection(db, 'sitedata'));
       querySnap.forEach((doc) => {
         if(doc.exists()) {
           const data = doc.data();
           if (data.lang === 'sp') {
             setSpanish(data);
+            setSpanishUpdated(data);
           } else {
             setEnglish(data);
+            setEnglishUpdated(data);
           }
         }
       });
-    
   }
 
   useEffect(() => {
     fetch();
   }, []);
+  
 
-  useEffect(() => {
-    console.log(spanish);
-    console.log(english);
-  }, [spanish, english]);
+  const handleSubmit = val => {
+    console.log(val);
+    //_.get(englishUpdated, val);
+  }
   
   return (
     <div>
       {
       english ? 
-      <div className="App">
-        <h1>{english.navbar.navlinks.about}</h1>
-        <h1>{spanish.navbar.navlinks.about}</h1>
+      <div className="App p-5">
+        <form>
+          <label className='m-3'>
+            {english.navbar.navlinks.home + ' : ' + englishUpdated.navbar.navlinks.home} 
+          </label>
+          <input></input>
+          <input type='submit' onSubmit={e => handleSubmit(e.target.value)}></input>
+        </form>
+        <h5>ORIG: {JSON.stringify(english.navbar.navlinks)}</h5>
+        <h5>UPDATED: {JSON.stringify(englishUpdated.navbar.navlinks)}</h5>
       </div> 
       : 
-      <div></div>
+      <div>LOADING</div>
     }
     </div>
     
